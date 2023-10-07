8a18e92eb8f9169c5645febcc30c38601730a1a2
erroronline1 <dev@erroronline.one>
2023-10-07 22:07:23 +0200

    ensure sql modes

--- api/api.php
+++ api/api.php
@@ -5,7 +5,8 @@ ini_set('display_errors', 1); error_reporting(E_ERROR);
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: text/html; charset=UTF-8');
 //database connection
-include_once('dbcredentials.php'); // contains just $pdo = new PDO('mysql:host=127.0.0.1;dbname=databasename;charset=utf8', 'username', 'password');
+include_once('dbcredentials.php'); // contains just $pdo = new PDO('mysql:host=127.0.0.1;dbname=webqs;charset=utf8', 'username', 'password');
+$pdo->exec("SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));");
 
 // always security logout
 if ($_SESSION["user"]){
@@ -26,10 +27,6 @@ function dbSanitize($text){
 function scriptFilter($text){
 	return htmlspecialchars(trim($text));
 }
-
-
-
-
 
 // read incoming stream
 $payload = json_decode(file_get_contents('php://input'));
