1351ebacb4775f25ec3b56b92080dfb9b06414ab
erroronline1 <dev@erroronline.one>
2023-10-21 22:31:21 +0200

    fixed signaturePad scope

--- js/assemble.js
+++ js/assemble.js
@@ -494,12 +494,11 @@ export class Assemble {
 	}
 }
 
-var canvas = null,
-	signaturePad = null;
+var canvas = null;
 
 function initialize_SignaturePad() {
 	canvas = document.getElementById("signaturecanvas");
-	signaturePad = new SignaturePad(canvas, {
+	window.signaturePad = new SignaturePad(canvas, {
 		// It's Necessary to use an opaque color when saving image as JPEG;
 		// this option can be omitted if only saving as PNG or SVG
 		//backgroundColor: 'rgb(255, 255, 255)'
@@ -508,7 +507,6 @@ function initialize_SignaturePad() {
 	// rather than window resize events.
 	window.onresize = resizeSignatureCanvas;
 	resizeSignatureCanvas();
-
 }
 // Adjust canvas coordinate space taking into account pixel ratio,
 // to make it look crisp on mobile devices.
@@ -518,19 +516,16 @@ function resizeSignatureCanvas() {
 	// some browsers report devicePixelRatio as less than 1
 	// and only part of the canvas is cleared then.
 	const ratio = Math.max(window.devicePixelRatio || 1, 1);
-
 	// This part causes the canvas to be cleared
 	canvas.width = canvas.offsetWidth * ratio;
 	canvas.height = canvas.offsetHeight * ratio;
 	canvas.getContext("2d").scale(ratio, ratio);
-
 	// This library does not listen for canvas changes, so after the canvas is automatically
 	// cleared by the browser, SignaturePad#isEmpty might still return false, even though the
 	// canvas looks empty, because the internal data of this library wasn't cleared. To make sure
 	// that the state of this library is consistent with visual state of the canvas, you
 	// have to clear it manually.
 	signaturePad.clear();
-
 	// If you want to keep the drawing on resize instead of clearing it you can reset the data.
 	signaturePad.fromData(signaturePad.toData());
 }
@@ -543,11 +538,9 @@ function dataURLToBlob(dataURL) {
 	const raw = window.atob(parts[1]);
 	const rawLength = raw.length;
 	const uInt8Array = new Uint8Array(rawLength);
-
 	for (let i = 0; i < rawLength; ++i) {
 		uInt8Array[i] = raw.charCodeAt(i);
 	}
-
 	return new Blob([uInt8Array], {
 		type: contentType
 	});
