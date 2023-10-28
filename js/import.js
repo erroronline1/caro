24d4eea5290ae14d0bf118c84dbd471d1021d22b
erroronline1 <dev@erroronline.one>
2023-10-28 17:36:05 +0200

    single place for imports and global scopes

---
+++ js/import.js
@@ -0,0 +1,23 @@
+//import relevant functions and set global scope
+
+import {
+    Assemble,
+    assemble_helper
+} from '../js/assemble.js';
+window.Assemble = Assemble;
+window.assemble_helper = assemble_helper;
+
+import {
+    api
+} from '../js/api.js';
+window.api = api;
+
+import {
+    Compose,
+    MetaCompose,
+    compose_helper
+} from '../js/compose.js';
+window.Compose = Compose;
+window.MetaCompose = MetaCompose;
+window.compose_helper = compose_helper;
+
