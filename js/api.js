5289ab303ef7450231ca1b83081da7e6ca0da96b
erroronline1 <dev@erroronline.one>
2023-10-28 17:37:39 +0200

    renamed

--- js/api.js
+++ js/api.js
@@ -1,9 +1,9 @@
 import {
 	Assemble
-} from './js/assemble.js';
+} from './assemble.js';
 import {
 	_
-} from './libraries/erroronline1.js';
+} from '../libraries/erroronline1.js';
 
 export const api = {
 	send: (method, payload, successFn, errorFn = null, form_data = false) => {
@@ -29,13 +29,47 @@ export const api = {
 		get form elements from database.
 		notice only the first requested form will appear. later duplicates will be ignored.
 		*/
-		let successFn = function (data) {
+		const successFn = function (data) {
 			new Assemble(data).initializeSection();
 		}
 		api.send('get', {
 			'request': 'getForms',
 			'content': [which, ...andMore].join(',')
 		}, successFn);
+	},
+	user: (request, id = null) => {
+		/*
+		user tasks
+		*/
+		const methods = {
+			'get': ['user_edit'],
+			'post': ['user_save'],
+			'delete': ['user_delete']
+		};
+		let successFn = function (data) {
+				document.getElementById('main').innerHTML = '';
+				new Assemble(data).initializeSection();
+			},
+			method,
+			payload = {
+				'request': request,
+				'id': id
+			};
+		for (let key in methods) {
+			if (methods[key].includes(request)) {
+				method = key;
+				break;
+			}
+		}
+		if (method === 'post') payload = _.getInputs('[data-usecase=user]', true);
+		if (method === 'post' || method === 'delete') {
+			successFn = function (data) {
+				console.trace();
+				console.log(request, method, data);
+				api.user('user_edit', data.id);
+			}
+		}
+		api.send(method, payload, successFn, null, method === 'post');
 	},
 	signIn: () => {
 
