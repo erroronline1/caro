d5cb27f7feb909748fbb2bde769f89850484a3d6
erroronline1 <dev@erroronline.one>
2023-12-22 22:03:20 +0100

    fixed text content pattern

--- js/assemble.js
+++ js/assemble.js
@@ -313,7 +313,7 @@ export class Assemble {
 			content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
 		}*/
 		
-		const content=this.tile.content.matchAll(/(.*?)(?:\n|\\n|<br \/>)/gm);
+		const content=this.tile.content.matchAll(/(.*?)(?:\n|\\n|<br \/>|$)/gm);
 		for (const part of content){
 			this.elements.add(document.createTextNode(part[1]));
 			this.elements.add(document.createElement('br'));
