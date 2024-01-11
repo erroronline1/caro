db6bde6a18d68bcfae3a0bccd602207fd0c68618
error on line 1 <dev@erroronline.one>
2024-01-11 14:14:41 +0100

    creating folders

--- api/file.php
+++ api/file.php
@@ -23,7 +23,6 @@ class FILE extends API {
 				if (!(array_intersect(['admin'], $_SESSION['user']['permissions']))) $this->response([], 401);
 				break;
 			case 'GET':
-				if (!(array_intersect(['admin', 'user'], $_SESSION['user']['permissions']))) $this->response([], 401);
 				break;
 			case 'DELETE':
 				if (!(array_intersect(['admin'], $_SESSION['user']['permissions']))) $this->response([], 401);
@@ -50,8 +49,59 @@ class FILE extends API {
 	}
 
 	public function manager(){
+		/**
+		 * no put method for windows server permissions are a pita
+		 * thus directories can not be renamed
+		 */
 		if (!(array_intersect(['admin'], $_SESSION['user']['permissions']))) $this->response([], 401);
+		switch ($_SERVER['REQUEST_METHOD']){
+			case 'POST':
+				$new_folder = preg_replace(['/[\s-]{1,}/', '/\W/'], ['_', ''], UTILITY::propertySet($this->_payload, str_replace(' ', '_', LANG::GET('file.new_folder'))));
+				if ($new_folder){
+					$new_folder = '../' . UTILITY::directory('files_documents', [':category' => $new_folder]);
+					UTILITY::storeUploadedFiles([], $new_folder);
+					$this->response(['status' => [
+						'msg' => LANG::GET('file.new_folder_created', [':name' => $new_folder])
+					]]);
+				}
+				break;
+			case 'GET':
+				$result=['body'=>
+				['form' => [
+					'data-usecase' => 'file',
+					'action' => "javascript:api.file('post', 'manager')"],
+				'content'=>[]]];
 
+				if (!$this->_requestedFolder){
+					$folders = UTILITY::listDirectories(UTILITY::directory('files_documents'),'asc');
+					if ($folders){
+						$content=[];
+						foreach ($folders as $folder){
+							$display = str_replace(UTILITY::directory('files_documents') . '/', '', $folder);
+							$content[$display] = ['href'=>"javascript:api.file('get', 'manager', '" . $display . "')"];
+						}
+						$result['body']['content'][]=[
+							['type' => 'links',
+							'description' => LANG::GET('file.created_folders'),
+							'content' => $content]
+						];
+					}
+					$result['body']['content'][]=[
+						['type' => 'textinput',
+						'description' => LANG::GET('file.new_folder'),
+						'attributes' => [
+							'required' => true]]
+					];
+				}
+				else {
+
+				}
+				$this->response($result);
+
+				break;
+			case 'DELETE':
+				break;
+		}
 	}
 }
 
