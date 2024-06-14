7acba2844bcfaef40758911434f40655b8127bcd
error on line 1 <dev@erroronline.one>
2024-06-14 14:44:52 +0200

    adding byte to counting

--- api/_linecounter.php
+++ api/_linecounter.php
@@ -1,10 +1,14 @@
 <?php
 $lines = 0;
+$byte = 0;
 foreach (['../', '../js', '../api'] as $dir){
     foreach (scandir($dir) as $file){
         if (!in_array(pathinfo($file)['extension'], ['php','ini','js','html','css','md'])) continue;
-        $lines += count(file($dir.'/'.$file));
+        foreach(file($dir.'/'.$file) as $row){
+            $lines++;
+            $byte+= strlen($row);
+        }
     }
 }
-echo $lines, ' lines';
+echo $lines, ' lines, ', $byte, ' byte';
 ?>
