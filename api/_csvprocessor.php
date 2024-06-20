2189d0734f63a42a506acb5e9ecdca5b4a393fa7
error on line 1 <dev@erroronline.one>
2024-06-20 08:28:05 +0200

    license

--- api/_csvprocessor.php
+++ api/_csvprocessor.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ * CARO - Cloud Assisted Records and Operations
+ * Copyright (C) 2023-2024 error on line 1 (dev@erroronline.one)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 /*
 filters and returns a named array according to setup.
 	if a filterset uses date thresholds or date intervals and month or year are not manually set,
