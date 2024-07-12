6aad04c78db5c05a54b521ba5d4dfa3d5f7f4603
erroronline1 <dev@erroronline.one>
2024-07-12 23:19:05 +0200

    moved mdr sample settings to lifespan

--- api/_sqlinterface.php
+++ api/_sqlinterface.php
@@ -329,19 +329,19 @@ class SQLQUERY {
 		],
 		'consumables_get_valid_checked' => [
 			'mysql' => "SELECT prod.vendor_id AS vendor_id FROM caro_consumables_products AS prod, caro_consumables_vendors AS dist WHERE prod.vendor_id = dist.id AND prod.trading_good = 1 "
-				."AND DATEDIFF( CURRENT_TIMESTAMP, IFNULL( prod.checked, DATE_SUB(CURRENT_TIMESTAMP, INTERVAL IFNULL( JSON_VALUE(dist.pricelist, '$.samplecheck_interval'), " . INI['limits']['mdr14_sample_interval'] . " ) DAY ) ) ) "
-				."< IFNULL( JSON_VALUE(dist.pricelist, '$.samplecheck_interval'), " . INI['limits']['mdr14_sample_interval'] . " )",
+				."AND DATEDIFF( CURRENT_TIMESTAMP, IFNULL( prod.checked, DATE_SUB(CURRENT_TIMESTAMP, INTERVAL IFNULL( JSON_VALUE(dist.pricelist, '$.samplecheck_interval'), " . INI['lifespan']['mdr14_sample_interval'] . " ) DAY ) ) ) "
+				."< IFNULL( JSON_VALUE(dist.pricelist, '$.samplecheck_interval'), " . INI['lifespan']['mdr14_sample_interval'] . " )",
 			'sqlsrv' => "SELECT prod.vendor_id FROM caro_consumables_products AS prod, caro_consumables_vendors AS dist WHERE prod.vendor_id = dist.id AND prod.trading_good = 1 AND "
-				."DATEDIFF(day, ISNULL(prod.checked, DATEADD(DD, ISNULL( JSON_VALUE(dist.pricelist, '$.samplecheck_interval'), " . INI['limits']['mdr14_sample_interval'] . ") * -1, GETDATE())), GETDATE()) "
-				."< ISNULL(JSON_VALUE(dist.pricelist, '$.samplecheck_interval'), " . INI['limits']['mdr14_sample_interval'] . ") "
+				."DATEDIFF(day, ISNULL(prod.checked, DATEADD(DD, ISNULL( JSON_VALUE(dist.pricelist, '$.samplecheck_interval'), " . INI['lifespan']['mdr14_sample_interval'] . ") * -1, GETDATE())), GETDATE()) "
+				."< ISNULL(JSON_VALUE(dist.pricelist, '$.samplecheck_interval'), " . INI['lifespan']['mdr14_sample_interval'] . ") "
 		],
 		'consumables_get_not_reusable_checked' => [
 			'mysql' => "SELECT prod.id AS id FROM caro_consumables_products AS prod, caro_consumables_vendors AS dist WHERE prod.vendor_id = dist.id AND prod.trading_good = 1 "
-				."AND DATEDIFF( CURRENT_TIMESTAMP, IFNULL( prod.checked, DATE_SUB( CURRENT_TIMESTAMP, INTERVAL IFNULL( JSON_VALUE(dist.pricelist, '$.samplecheck_reusable'), " . INI['limits']['mdr14_sample_reusable'] . " ) + 1 DAY ) ) ) "
-				."< IFNULL( JSON_VALUE(dist.pricelist, '$.samplecheck_reusable'), " . INI['limits']['mdr14_sample_reusable'] . " )",
+				."AND DATEDIFF( CURRENT_TIMESTAMP, IFNULL( prod.checked, DATE_SUB( CURRENT_TIMESTAMP, INTERVAL IFNULL( JSON_VALUE(dist.pricelist, '$.samplecheck_reusable'), " . INI['lifespan']['mdr14_sample_reusable'] . " ) + 1 DAY ) ) ) "
+				."< IFNULL( JSON_VALUE(dist.pricelist, '$.samplecheck_reusable'), " . INI['lifespan']['mdr14_sample_reusable'] . " )",
 			'sqlsrv' => "SELECT prod.id FROM caro_consumables_products AS prod, caro_consumables_vendors AS dist WHERE prod.vendor_id = dist.id AND prod.trading_good = 1 AND "
-				."DATEDIFF(day, ISNULL(prod.checked, DATEADD(DD, ISNULL(JSON_VALUE(dist.pricelist, '$.samplecheck_reusable'), " . INI['limits']['mdr14_sample_reusable'] . ") * -1 - 1, GETDATE())), GETDATE()) "
-				."< ISNULL(JSON_VALUE(dist.pricelist, '$.samplecheck_reusable'), " . INI['limits']['mdr14_sample_reusable'] . ") "
+				."DATEDIFF(day, ISNULL(prod.checked, DATEADD(DD, ISNULL(JSON_VALUE(dist.pricelist, '$.samplecheck_reusable'), " . INI['lifespan']['mdr14_sample_reusable'] . ") * -1 - 1, GETDATE())), GETDATE()) "
+				."< ISNULL(JSON_VALUE(dist.pricelist, '$.samplecheck_reusable'), " . INI['lifespan']['mdr14_sample_reusable'] . ") "
 		],
 		'consumables_get_last_checked' => [
 			'mysql' => "SELECT prod.checked as checked, dist.id as vendor_id FROM caro_consumables_products AS prod, caro_consumables_vendors as dist WHERE prod.trading_good = 1 AND prod.vendor_id = dist.id AND "
--- api/consumables.php
+++ api/consumables.php
@@ -658,8 +658,8 @@ class CONSUMABLES extends API {
 				$vendor['certificate']['validity'] = UTILITY::propertySet($this->_payload, LANG::PROPERTY('consumables.edit_vendor_certificate_validity'));
 				$vendor['pricelist'] = json_decode($vendor['pricelist'], true);
 				$vendor['pricelist']['filter'] = UTILITY::propertySet($this->_payload, LANG::PROPERTY('consumables.edit_vendor_pricelist_filter'));
-				$vendor['pricelist']['samplecheck_interval'] = UTILITY::propertySet($this->_payload, LANG::PROPERTY('consumables.edit_vendor_samplecheck_interval')) ? : INI['limits']['mdr14_sample_interval'];
-				$vendor['pricelist']['samplecheck_reusable'] = UTILITY::propertySet($this->_payload, LANG::PROPERTY('consumables.edit_vendor_samplecheck_interval_reusable')) ? : INI['limits']['mdr14_sample_reusable'];
+				$vendor['pricelist']['samplecheck_interval'] = UTILITY::propertySet($this->_payload, LANG::PROPERTY('consumables.edit_vendor_samplecheck_interval')) ? : INI['lifespan']['mdr14_sample_interval'];
+				$vendor['pricelist']['samplecheck_reusable'] = UTILITY::propertySet($this->_payload, LANG::PROPERTY('consumables.edit_vendor_samplecheck_interval_reusable')) ? : INI['lifespan']['mdr14_sample_reusable'];
 
 				foreach(INI['forbidden']['names'] as $pattern){
 					if (preg_match("/" . $pattern . "/m", $vendor['name'], $matches)) $this->response(['response' => ['msg' => LANG::GET('consumables.error_vendor_forbidden_name', [':name' => $vendor['name']]), 'type' => 'error']]);
@@ -1006,14 +1006,14 @@ class CONSUMABLES extends API {
 								'type' => 'number',
 								'attributes' => [
 									'name' => LANG::GET('consumables.edit_vendor_samplecheck_interval'),
-									'value' => array_key_exists('samplecheck_interval', $vendor['pricelist']) ? $vendor['pricelist']['samplecheck_interval'] : INI['limits']['mdr14_sample_interval']
+									'value' => array_key_exists('samplecheck_interval', $vendor['pricelist']) ? $vendor['pricelist']['samplecheck_interval'] : INI['lifespan']['mdr14_sample_interval']
 								]
 							],
 							[
 								'type' => 'number',
 								'attributes' => [
 									'name' => LANG::GET('consumables.edit_vendor_samplecheck_interval_reusable'),
-									'value' => array_key_exists('samplecheck_reusable', $vendor['pricelist']) ? $vendor['pricelist']['samplecheck_reusable'] : INI['limits']['mdr14_sample_reusable']
+									'value' => array_key_exists('samplecheck_reusable', $vendor['pricelist']) ? $vendor['pricelist']['samplecheck_reusable'] : INI['lifespan']['mdr14_sample_reusable']
 								]
 							]
 						]]
