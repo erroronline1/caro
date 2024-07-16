ec5cf94054eff8558e149c39aa7e26ef61eda924
erroronline1 <dev@erroronline.one>
2024-07-16 23:13:36 +0200

    added application elements to rendertest

--- js/unittests.js
+++ js/unittests.js
@@ -151,91 +151,154 @@ const oldform2 = {
 	]
 };
 */
-var unittestCreateForm = {
-	content: [
-		[
-			{
-				type: "scanner",
-				attributes: {
-					name: "Barcode scanner",
-					multiple: true,
-				},
-			},
-			{
-				type: "textblock",
-				description: "Informative text",
-				content: "...to inform users about important things",
-			},
-			{ type: "file", attributes: { name: "Add an upload for files" }, hint: "You can opt for multiple file selection" },
-			{ type: "photo", attributes: { name: "Contribute photos right from your camera" }, hint: "Desktop opens another file dialog though" },
-			{ type: "signature", attributes: { name: "Add a signature pad" }, hint: "Unfortunately this is not certified, so keep a good measure" },
-		],
-		[
-			{
-				type: "text",
-				attributes: {
-					name: "A simple text input",
-				},
-				hint: "Clarify with hints",
-			},
-			{
-				type: "textarea",
-				attributes: { name: "A multiline text input field", value: "The button opens the templates where you can assemble your text, copy and paste it here" },
-				texttemplates: true,
-				hint: "With optional access to predefined text templates",
-			},
-			{ type: "number", attributes: { name: "A number input" }, hint: "By the way: all fields can be optional set to be required, so storing the form is possible with provided input only!" },
-			{ type: "date", attributes: { name: "A date field makes input easy with the host systems date selection" } },
-			{ type: "tel", attributes: { name: "The phone number field displays a keypad on mobile" } },
-			{ type: "email", attributes: { name: "An email field does it similar" } },
-		],
-		[
-			{
-				type: "links",
-				description: "Links can be added as well",
-				content: {
-					"https://www.mozilla.org/en-UD/firefox": { href: "https://www.mozilla.org/en-UD/firefox" },
-					"https://www.microsoft.com/en-us/edge": { href: "https://www.microsoft.com/en-us/edge" },
-					"https://www.google.com/intl/en/chrome": { href: "https://www.google.com/intl/en/chrome" },
-				},
-				hint: "Popular browsers",
-			},
-		],
-		[
-			{ type: "range", attributes: { min: 0, max: 10, value: 3, name: "An easy slider" }, hint: "You can define steps as well" },
-			{ type: "checkbox", description: "Select from multiple options at a glance", hint: "Best used with less than 8 for a better user experience", content: { "This is great": {}, "This is awesome": {}, "This is superb": { checked: true } } },
-			{
-				type: "radio",
-				attributes: { name: "Select only one option from a few at a glance" },
-				hint: "Less than 8 recommended as well",
-				content: {
-					"Either this": {},
-					"Or this": { checked: true },
-					"Or even this": {},
-				},
-			},
-			{
-				type: "select",
-				attributes: {
-					name: "Select one option of many",
-				},
-				hint: "This opens a longer list that yould otherwise clutter up the screen",
-				content: {
-					"Selection one": {},
-					"Selection two": {},
-					"Selection three": {},
-				},
-			},
-		],
-		[
-			{ type: "calendarbutton", attributes: { value: "Add event to calendar" } },
-			{ type: "formbutton", attributes: { value: "Display SILLY FORM" } },
-		],
-	],
-};
 
-export function rendertest() {
-	const render = new Assemble(unittestCreateForm);
+export function rendertest(element) {
+	let tests = {
+		forms: {
+			content: [
+				[
+					{
+						type: "scanner",
+						attributes: {
+							name: "Barcode scanner",
+							multiple: true,
+						},
+					},
+					{
+						type: "textblock",
+						description: "Informative text",
+						content: "...to inform users about important things",
+					},
+					{ type: "file", attributes: { name: "Add an upload for files" }, hint: "You can opt for multiple file selection" },
+					{ type: "photo", attributes: { name: "Contribute photos right from your camera" }, hint: "Desktop opens another file dialog though" },
+					{ type: "signature", attributes: { name: "Add a signature pad" }, hint: "Unfortunately this is not certified, so keep a good measure" },
+				],
+				[
+					{
+						type: "text",
+						attributes: {
+							name: "A simple text input",
+						},
+						hint: "Clarify with hints",
+					},
+					{
+						type: "textarea",
+						attributes: { name: "A multiline text input field", value: "The button opens the templates where you can assemble your text, copy and paste it here" },
+						texttemplates: true,
+						hint: "With optional access to predefined text templates",
+					},
+					{ type: "number", attributes: { name: "A number input" }, hint: "By the way: all fields can be optional set to be required, so storing the form is possible with provided input only!" },
+					{ type: "date", attributes: { name: "A date field makes input easy with the host systems date selection" } },
+					{ type: "tel", attributes: { name: "The phone number field displays a keypad on mobile" } },
+					{ type: "email", attributes: { name: "An email field does it similar" } },
+				],
+				[
+					{
+						type: "links",
+						description: "Links can be added as well",
+						content: {
+							"https://www.mozilla.org/en-UD/firefox": { href: "https://www.mozilla.org/en-UD/firefox" },
+							"https://www.microsoft.com/en-us/edge": { href: "https://www.microsoft.com/en-us/edge" },
+							"https://www.google.com/intl/en/chrome": { href: "https://www.google.com/intl/en/chrome" },
+						},
+						hint: "Popular browsers",
+					},
+				],
+				[
+					{ type: "range", attributes: { min: 0, max: 10, value: 3, name: "An easy slider" }, hint: "You can define steps as well" },
+					{
+						type: "checkbox",
+						description: "Select from multiple options at a glance",
+						hint: "Best used with less than 8 for a better user experience",
+						content: { "This is great": {}, "This is awesome": {}, "This is superb": { checked: true } },
+					},
+					{
+						type: "radio",
+						attributes: { name: "Select only one option from a few at a glance" },
+						hint: "Less than 8 recommended as well",
+						content: {
+							"Either this": {},
+							"Or this": { checked: true },
+							"Or even this": {},
+						},
+					},
+					{
+						type: "select",
+						attributes: {
+							name: "Select one option of many",
+						},
+						hint: "This opens a longer list that yould otherwise clutter up the screen",
+						content: {
+							"Selection one": {},
+							"Selection two": {},
+							"Selection three": {},
+						},
+					},
+				],
+				[
+					{ type: "calendarbutton", attributes: { value: "Add event to calendar" } },
+					{ type: "formbutton", attributes: { value: "Display SILLY FORM" } },
+				],
+			],
+		},
+		app: {
+			content: [
+				[
+					{
+						type: "time",
+						attributes: {
+							name: "Time input",
+						},
+						hint: "Easy selection with hosts time picker, if available",
+					},
+					{
+						type: "search",
+						attributes: { name: "Search input" },
+						hint: "Search keyboard and looking glass icon",
+					},
+					{ type: "filtered", attributes: { name: "Filter input" }, hint: "Search keyboard and funnel icon" },
+					{
+						type: "checkbox2text",
+						attributes: { name: "Selected modal checkboxes are chained comma separated, onblur" },
+						hint: "Makes selections comprehensible while providing a single payload object",
+						content: { One: { value: "1" }, Two: { value: 2 }, Three: { value: "Three" } },
+					},
+					{ type: "range", attributes: { name: "Range with datalist", list: "range_datalist", min: 0, max: 4, value: 2 } },
+					{ type: "datalist", attributes: { id: "range_datalist", class: "rangedatalist" }, content: [{ label: "A" }, { label: "range" }, { label: "with" }, { label: "a" }, { label: "datalist" }] },
+					{ type: "datalist", attributes: { id: "text_datalist" }, content: ["A", "textinput", "with", "a", "datalist"] },
+					{ type: "text", attributes: { name: "Text input with datalist", list: "text_datalist" } },
+				],
+				[
+					{
+						type: "button",
+						attributes: {
+							value: "A generic button",
+							type: "button",
+						},
+						hint: "Buttons can have hint too!",
+					},
+					{
+						type: "deletebutton",
+						attributes: { value: "A delete button", type: "button" },
+					},
+					{
+						type: "submitbutton",
+						attributes: { value: "A submit button", type: "button" },
+						hint: "Submits the parent form",
+					},
+				],
+				[
+					{ type: "tile", content: [{ type: "textblock", description: "Textblock on a tile", content: "This is a third width" }] },
+					{ type: "tile", content: [{ type: "text", attributes: { name: "Any element is possible" }, hint: "Not necessarily sensible though." }] },
+				],
+				[
+					[{ type: "textblock", description: "Sliding panels are possible as well", content: "Just try not to distract or obfuscate. Mobile view is just swiping, desktop view has supporting buttons." }],
+					[{ type: "textblock", description: "It's just another layer of nesting", content: "All options above are valid" }],
+				],
+			],
+		},
+	};
+	const render = new Assemble(tests[element]);
 	document.getElementById("main").replaceChildren(render.initializeSection());
 	render.processAfterInsertion();
 }
