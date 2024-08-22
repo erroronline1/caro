4e25d19ef909b330242a446f48081e6e153f40d5
error on line 1 <dev@erroronline.one>
2024-08-22 10:59:00 +0200

    more info

--- api/_linecounter.php
+++ api/_linecounter.php
@@ -1,14 +1,18 @@
 <?php
 $lines = 0;
 $byte = 0;
+$files = 0;
 foreach (['../', '../js', '../api'] as $dir){
     foreach (scandir($dir) as $file){
         if (!in_array(pathinfo($file)['extension'], ['php','ini','js','html','css','md'])) continue;
+        $files++;
         foreach(file($dir.'/'.$file) as $row){
             $lines++;
             $byte+= strlen($row);
         }
     }
 }
-echo $lines, ' lines, ', $byte, ' byte';
+echo $lines, ' lines, ', $byte, ' byte, ', $files, ' files';
+
+//phpinfo();
 ?>
