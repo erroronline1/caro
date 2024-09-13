86774b9c940b72d83f16511d384527e30536720d
erroronline1 <dev@erroronline.one>
2024-09-13 21:02:31 +0200

    categorizing lines

--- api/_linecounter.php
+++ api/_linecounter.php
@@ -1,18 +1,29 @@
 <?php
-$lines = 0;
+$lines = ['frontend' => 0,'backend' => 0, 'code' => 0, 'documentation' => 0];
 $byte = 0;
 $files = 0;
 foreach (['../', '../js', '../api'] as $dir){
 	foreach (scandir($dir) as $file){
-		if (!in_array(pathinfo($file)['extension'], ['php','ini','js','html','css','md'])) continue;
+		if (!isset(pathinfo($file)['extension']) || !in_array(pathinfo($file)['extension'], ['php','ini','js','html','css','md'])) continue;
 		$files++;
 		foreach(file($dir.'/'.$file) as $row){
-			$lines++;
+			if (in_array(pathinfo($file)['extension'], ['php','ini'])){
+				$lines['backend']++;
+				$lines['code']++;
+			}
+			if (in_array(pathinfo($file)['extension'], ['js','html','css'])){
+				$lines['frontend']++;
+				$lines['code']++;
+			}
+			if (in_array(pathinfo($file)['extension'], ['md'])){
+				$lines['documentation']++;				
+			}
 			$byte+= strlen($row);
 		}
 	}
 }
-echo $lines, ' lines, ', $byte, ' byte, ', $files, ' files<br />';
+var_dump($lines);
+echo '<br />', $lines['code']+$lines['documentation'], ' lines, ', $byte, ' byte, ', $files, ' files<br />';
 
 //phpinfo();
 ?>
