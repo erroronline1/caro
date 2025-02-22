089b0c7c8f3c0a3d900568feeb7006a11ef5c618
erroronline1 <dev@erroronline.one>
2025-02-22 01:39:32 +0100

    fixed type specific comparison on removing audittemplates

--- api/_stresstest.php
+++ api/_stresstest.php
@@ -297,7 +297,7 @@ class STRESSTEST extends INSTALL{
 				if (
 					isset($entry['unit']) &&
 					$dbentry['unit'] === $entry['unit'] &&
-					$dbentry['hint'] === $entry['hint'] &&
+					$dbentry['hint'] == $entry['hint'] && // empty !== null
 					$dbentry['objectives'] === $entry['objectives']
 					// no checking if $dbentry['content'] === $entry['content'] for db-specific character encoding
 				){
