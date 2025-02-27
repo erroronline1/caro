ba619175ffe763d7d562ecbefaa3f121d84d1ef7
error on line 1 <dev@erroronline.one>
2025-02-27 07:34:15 +0100

    removed debugging output

--- api/_calendarutility.php
+++ api/_calendarutility.php
@@ -740,7 +740,6 @@ class CALENDARUTILITY {
 					foreach($settingentries as $line){
 						// match ISO 8601 start date of contract settings, days of annual vacation or weekly hours
 						preg_match('/(\d{4}.\d{2}.\d{2}).+?([\d,\.]+)/', $line, $lineentry);
-						var_dump($line, $lineentry);
 						// append datetime value and contract value
 						$hours_vacation[] = ['date' => new DateTime($lineentry[1], $datetimezone), 'value' => floatval(str_replace(',', '.', $lineentry[2]))];
 					}
