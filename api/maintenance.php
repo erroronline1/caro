f00d6614c6da8ba079f5a9e4cd2876677c87c64a
erroronline1 <dev@erroronline.one>
2025-07-12 20:34:48 +0200

    implemented vendor update in maintenance

--- api/maintenance.php
+++ api/maintenance.php
@@ -90,7 +90,7 @@ class MAINTENANCE extends API {
 	 * entry point
 	 */ 
 	public function task(){
-		$response['render'] = ['content' => []];
+		$response = ['render' => ['content' => []]];
 		$selecttypes = ['...' => []];
 		
 		foreach ([
@@ -115,6 +115,13 @@ class MAINTENANCE extends API {
 
 		if ($this->_requestedType && $this->_requestedType !== '...') {
 			if ($append = $this->{$this->_requestedType}()) array_push($response['render']['content'] , ...$append);
+
+			if (in_array($this->_requestedType, ['records_datalist', 'vendorupdate']) && $_SERVER['REQUEST_METHOD'] !== 'PUT'){
+				$response['render']['form'] = [
+					'data-usecase' => 'maintenance',
+					'action' => "javascript:api.maintenance('" . ($_SERVER['REQUEST_METHOD'] === 'GET' ? 'post' : 'put') . "', 'task', '" . $this->_requestedType . "')"
+				];
+			}
 		}
 		$this->response($response);
 	}
@@ -151,16 +158,159 @@ class MAINTENANCE extends API {
 	 * this is only somewhat a duplicate of install.php since the template file can be uploaded to not be reliant on server access
 	 */
 	private function vendorupdate(){
+		$content = [];
 		switch ($_SERVER['REQUEST_METHOD']){
 			case 'POST':
 				// store within temp to remain for put action, display matches and selection
+				if (isset($_FILES[$this->_lang->PROPERTY('maintenance.vendorupdate.file')]) && $_FILES[$this->_lang->PROPERTY('maintenance.vendorupdate.file')]['tmp_name']) {
+					$file = UTILITY::storeUploadedFiles([$this->_lang->PROPERTY('maintenance.vendorupdate.file')], UTILITY::directory('tmp'), [], [$_SESSION['user']['id'] . 'vendorupdate'], true);
+					$json = file_get_contents($file[0]);
+					$json = json_decode($json, true);
+					if (!$json)	{
+						return [[
+							'type' => 'textsection',
+							'attributes' => [
+								'name' => $this->_lang->GET('maintenance.vendorupdate.format_error'),
+								'class' => 'red'
+							]
+						]];
+					}
+					$content[] = [
+							'type' => 'textsection',
+							'attributes' => [
+								'name' => $this->_lang->GET('maintenance.vendorupdate.select_description'),
+							]
+					];
+					// gather possibly existing entries
+					$DBall = [
+						...SQLQUERY::EXECUTE($this->_pdo, 'consumables_get_vendor_datalist'),
+					];
+					$intersections = array_intersect(array_column($DBall, 'name'), array_column($json, 'name'));
+					$options = [];
+					foreach ($DBall as $vendor){
+						if (!in_array($vendor['name'], $intersections)) continue;
+						$options[] = [
+							'type' => 'checkbox',
+							'attributes' => [
+								'name' => $vendor['name']
+							],
+							'hint' => $vendor['hidden'] ? $this->_lang->GET('maintenance.vendorupdate.vendor_hidden') : null,
+							'content' => [
+								$this->_lang->GET('maintenance.vendorupdate.update_info') => [],
+								$this->_lang->GET('maintenance.vendorupdate.update_pricelist') => [],
+							]
+						];
+					}
+					if ($options) $content[] = $options;
+					if ($missing = array_diff(array_column($DBall, 'name'), $intersections)){
+						$content[] = [
+							'type' => 'textsection',
+							'attributes' => [
+								'name' => $this->_lang->GET('maintenance.vendorupdate.vendor_missing'),
+							],
+							'content' => implode(', ', $missing)
+						];
+					}
+				}
+				else {
+					return [[
+						'type' => 'textsection',
+						'attributes' => [
+							'name' => $this->_lang->GET('maintenance.vendorupdate.file_error'),
+							'class' => 'red'
+						]
+					]];
+				}
 				break;
 			case 'PUT':
 				// actual update vendors as per selection
+
+				// no exception handling, since the file validity has been checked upon upload already
+				$json = file_get_contents(UTILITY::directory('tmp') . '/' . $_SESSION['user']['id'] . 'vendorupdate.json');
+				$json = json_decode($json, true);
+				$DBall = [
+					...SQLQUERY::EXECUTE($this->_pdo, 'consumables_get_vendor_datalist'),
+				];
+				$intersections = array_intersect(array_column($DBall, 'name'), array_column($json, 'name'));
+				
+				$sqlchunks = [];
+				$success = [];
+				foreach($this->_payload as $key => $value){
+					if(!in_array($key, $intersections)) continue;
+
+					$vendor = $DBall[array_search($key, array_column($DBall, 'name'))];
+					$entry = $json[array_search($key, array_column($json, 'name'))];
+					$selected = explode(' | ', $value);
+					// sanitize options being numerated
+					foreach($selected as &$selection){
+						$selection = preg_replace('/\(\d+\)$/', '', $selection);
+					}
+
+					if (in_array($this->_lang->GET('maintenance.vendorupdate.update_info'), $selected)){
+						$vendor['info'] = isset($entry['info']) && gettype($entry['info']) === 'array' ? UTILITY::json_encode($entry['info']) : $vendor['info'];
+					}
+					if (in_array($this->_lang->GET('maintenance.vendorupdate.update_pricelist'), $selected)) {
+						$vendor['pricelist'] = json_decode($vendor['pricelist'] ? : '', true);
+						$newpricelistfilter = (isset($entry['pricelist']) && gettype($entry['pricelist']) === 'array' && isset($entry['pricelist']['filter'])) ? UTILITY::json_encode($entry['pricelist']['filter'], JSON_PRETTY_PRINT) : null;
+						$vendor['pricelist']['filter'] = $newpricelistfilter ? : (isset($vendor['pricelist']['filter']) ? $vendor['pricelist']['filter'] : null);
+						if(isset($entry['pricelist']['samplecheck_interval']) && $entry['pricelist']['samplecheck_interval']) $vendor['pricelist']['samplecheck_interval'] = $entry['pricelist']['samplecheck_interval'];
+						if(isset($entry['pricelist']['samplecheck_reusable']) && $entry['pricelist']['samplecheck_reusable']) $vendor['pricelist']['samplecheck_reusable'] = $entry['pricelist']['samplecheck_reusable'];
+
+						$vendor['pricelist'] = UTILITY::json_encode($vendor['pricelist'], JSON_PRETTY_PRINT);
+					}
+
+					$sqlchunks = SQLQUERY::CHUNKIFY($sqlchunks, strtr(SQLQUERY::PREPARE('consumables_put_vendor'),
+						[
+							':id' => $vendor['id'],
+							':name' => $this->_pdo->quote($vendor['name']),
+							':evaluation' => $vendor['evaluation'] ? $this->_pdo->quote($vendor['evaluation']) : 'NULL',
+							':hidden' => $vendor['hidden'] ? $this->_pdo->quote($vendor['hidden']) : 'NULL',
+							':certificate' => $vendor['certificate'] ? $this->_pdo->quote($vendor['certificate']) : 'NULL',
+							':info' => $vendor['info'] ? $this->_pdo->quote($vendor['info']) : 'NULL',
+							':pricelist' => $vendor['pricelist'] ? $this->_pdo->quote($vendor['pricelist']) : 'NULL',
+						]) . '; ');
+					$success[] = $vendor['name'];
+				}
+				foreach ($sqlchunks as $chunk){
+					try {
+						SQLQUERY::EXECUTE($this->_pdo, $chunk);
+					}
+					catch (\Exception $e) {
+						return [[
+							'type' => 'textsection',
+							'attributes' => [
+								'name' => $this->_lang->GET('maintenance.vendorupdate.update_error'),
+							],
+							'content' => $e,
+						]];
+					}
+				}
+				if ($success) return [[
+					'type' => 'textsection',
+					'attributes' => [
+						'name' => $this->_lang->GET('maintenance.vendorupdate.update_success'),
+					],
+					'content' => implode('\n', $success),
+				]];
 				break;
 			case "GET": 
 				// upload form
+				$content[] = [
+					'type' => 'textsection',
+					'attributes' => [
+						'name' => $this->_lang->GET('maintenance.navigation.vendorupdate'),
+					],
+					'content' => $this->_lang->GET('maintenance.vendorupdate.description'),
+				];
+				$content[] = [
+					'type' => 'file',
+					'attributes' => [
+						'required'=> true,
+						'name' => $this->_lang->GET('maintenance.vendorupdate.file'),
+					] 
+				];
 		}
+		return $content;
 	}
 }
 ?>
