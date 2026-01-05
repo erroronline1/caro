20ea762fcf1415f2477b258413d21fc5499fdcbb
error on line 1 <dev@erroronline.one>
2026-01-05 08:05:33 +0100

    ensure order data ist processed correctly

--- api/_sandbox.php
+++ api/_sandbox.php
@@ -2,28 +2,9 @@
 namespace CARO\API;
 require_once('_config.php');
 require_once('_utility.php'); // general utilities
+require_once('_erpinterface.php');
 
-$x = new BLOCKCHAIN;
-$y = [
-    'test'=>123
-];
-$z = $x->add([],$y);
-// static class can be instatiated to a variable too
-//var_dump($z);
-//var_dump($x->verified($z, true));
-
-
-$reflector = new \ReflectionMethod('CARO\API\BLOCKCHAIN::verified');
-$reflector_start = $reflector->getStartLine()-1;
-$reflector_end = $reflector->getEndLine()-1;
-echo $reflector->__toString();
-
-foreach(file('_utility.php') as $line => $codeline){
-    if ($line < $reflector_start) continue;
-    echo $codeline;
-    if ($line > $reflector_end) break;
-}
-
+var_dump(ERPINTERFACE->orderdata());
 
 
 //phpinfo();
