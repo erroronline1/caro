a3ce7830591f61ab44fb796ead016eee0f4dac22
error on line 1 <dev@erroronline.one>
2026-02-02 07:44:29 +0100

    hide errors from faulty windows handling temp directory

--- api/api.php
+++ api/api.php
@@ -16,7 +16,7 @@ session_set_cookie_params([
 	'secure' => true,
 	'httponly' => true,
 ]);
-session_start();
+@session_start();
 
 ini_set('display_errors', 1); error_reporting(E_ALL);
 header('Access-Control-Allow-Origin: *');
