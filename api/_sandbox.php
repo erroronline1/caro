830f7e945c3c4b4f91938fda6b4ecdd72e5897de
error on line 1 <dev@erroronline.one>
2026-03-04 07:38:06 +0100

    watermark pattern experiments

--- api/_sandbox.php
+++ api/_sandbox.php
@@ -5,6 +5,155 @@ require_once('_utility.php'); // general utilities
 //require_once('_erpinterface.php');
 //var_dump(ERPINTERFACE->orderdata());
 
+function alterImage($file, $maxSize = 1024, $destination = UTILITY_IMAGE_REPLACE, $forceOutputType = false, $label = '', $watermark = '', $watermarkPattern = false){
+		if (is_file($file)){
+			$filetype = getimagesize($file)[2];
+			switch($filetype){
+				case "1": //gif
+					$input = imagecreatefromgif($file);
+					break;
+				case "2": //jpeg
+					$input = imagecreatefromjpeg($file);
+					break;
+				case "3": //png
+					$input = imagecreatefrompng($file);
+					break;
+			}
+		}
+		else $input = imagecreatefromstring($file); // bytestring
+		if ($input) {
+			$filename = pathinfo($file)['basename'];
+			// resizing
+			$src = ['w' => imagesx($input), 'h' => imagesy($input)];
+			if ($maxSize > 0 && $src['w'] >= $src['h'] && $src['w'] > $maxSize) $resize = $maxSize / $src['w'];
+			elseif ($maxSize > 0 && $src['w'] < $src['h'] && $src['h'] > $maxSize) $resize = $maxSize / $src['h'];
+			else $resize = 1;
+			$new = ['w' => ceil($src['w'] * $resize), 'h' => ceil($src['h'] * $resize)];
+			$output = imagecreatetruecolor($new['w'], $new['h']);
+			imagefill($output, 0, 0, imagecolorallocatealpha($input, 0, 0, 0, 127));
+			imagealphablending($output, false);
+			imagesavealpha($output, true);
+			imagecolortransparent($output, imagecolorallocate($output, 0, 0, 0));
+			imagecopyresampled($output, $input, 0, 0, 0, 0, $new['w'], $new['h'], $src['w'], $src['h']);
+
+			// patterned watermark
+			if ($watermarkPattern){
+				imagealphablending($output, true);
+
+				// load pattern and make it more transparent
+				$tileSrc = imagecreatefrompng('../media/favicon/watermarkpattern.png');
+				imagealphablending($tileSrc, false);
+				imagesavealpha($tileSrc, true);
+				imagefilter($tileSrc, IMG_FILTER_COLORIZE,0,0,0,72);
+
+				// create smaller pattern ressource
+				$tile = imagecreatetruecolor(10,10);
+				imagefill($tile, 0, 0, imagecolorallocatealpha($tile, 0, 0, 0, 127));
+				imagecolortransparent($tile, imagecolorallocate($tile, 0, 0, 0));
+				imagecopyresampled($tile, $tileSrc, 0, 0, 0, 0, 10, 10, 88, 72);
+
+				// apply tiled pattern
+				imagesettile($output, $tile);
+				imagefilledrectangle($output, 0, 0, $new['w'], $new['h'], IMG_COLOR_TILED);
+
+				imagealphablending($output, false);
+			}
+
+			$scale = .15; // of shorter length
+			$opacity = 20; // for watermark
+			// labelling
+			if ($label){
+				if ($new['w'] >= $new['h']) $height = $new['h'] * $scale / 1.5;
+				else $height = $new['w'] * $scale / 1.5;
+				$height = ceil($height);
+				$input = imagecreatetruecolor($new['w'], $height);
+				imagecolortransparent($input, imagecolorallocate($input, 0, 0, 0));
+				$textcolor = imagecolorallocate($input, 1, 1, 1);
+				imagefttext($input, $height / 2, 0, ceil($height / 5), $height - ceil($height / 7), $textcolor,  __DIR__ . '/../media/UbuntuMono-R.ttf', $label);
+				$textcolor = imagecolorallocate($input, 255, 255, 255);
+				imagefttext($input, $height / 2, 0, ceil($height / 6), $height - ceil($height / 6), $textcolor,  __DIR__ . '/../media/UbuntuMono-R.ttf', $label);
+				imagecopymerge($output, $input, 0, $new['h'] - $height, 0, 0, $new['w'], $new['h'], 99);
+			}
+
+			// watermark on lower right
+			if ($watermark && is_file($watermark)){
+				$filetype = getimagesize($watermark)[2];
+				switch($filetype){
+					case "1": //gif
+						$input = imagecreatefromgif($watermark);
+						break;
+					case "2": //jpeg
+						$input = imagecreatefromjpeg($watermark);
+						break;
+					case "3": //png
+						$input = imagecreatefrompng($watermark);
+						break;
+				}
+				$wm = ['w' => imagesx($input), 'h' => imagesy($input)];
+				if ($new['w'] >= $new['h']) $resize = $new['h'] * $scale / $wm['w'];
+				else $resize = $new['w'] * $scale / $wm['h'];
+				$newwm = ['w' => ceil($wm['w'] * $resize), 'h' => ceil($wm['h'] * $resize)];
+				$stamp = imagecreatetruecolor($newwm['w'], $newwm['h']);
+				imagecolortransparent($stamp, imagecolorallocate($stamp, 0, 0, 0));
+				imagecopyresampled($stamp, $input, 0, 0, 0, 0, $newwm['w'], $newwm['h'], $wm['w'], $wm['h']);
+
+				imagecopymerge($output, $stamp, $new['w'] - $newwm['w'], $new['h'] - $newwm['h'], 0, 0, $newwm['w'], $newwm['h'], $opacity);
+			}
+	
+			if ($destination & UTILITY_IMAGE_REPLACE){
+				chmod($file, 0777);
+				switch($filetype){
+					case "1": //gif
+						imagegif($output, $file);
+						break;
+					case "2": //jpeg
+						imagejpeg($output, $file, 100);
+						break;
+					case "3": //png
+						imagepng($output, $file, 0);
+						break;
+				}
+				return;
+			}
+
+			if ($forceOutputType){
+				$newtype = array_search($forceOutputType, ['gif', 'jpeg', 'png', 'jpg']);
+				if ($newtype !== false){
+					if ($newtype == 3) $newtype = 1;
+					$filetype = $newtype + 1;
+					$filename = pathinfo($file)['filename'] . $forceOutputType;
+				}
+			}
+
+			if ($destination & UTILITY_IMAGE_STREAM) {
+					header("Content-type: application/octet-stream");
+					header("Content-Disposition: attachment; filename=" . $filename);
+			}
+			ob_start();	
+			switch($filetype){
+				case "1": //gif
+					imagegif($output, null);
+					break;
+				case "2": //jpeg
+					imagejpeg($output, null, 100);
+					break;
+				case "3": //png
+					imagepng($output, null, 6);
+					break;
+			}
+			if ($destination & UTILITY_IMAGE_STREAM) {
+				ob_flush();
+			}
+			$return = ob_get_contents();
+			ob_end_clean(); 
+			return $return;
+		}
+}
+
+
+alterImage('../media/developer.png', 1024, UTILITY_IMAGE_STREAM, false, '', '', true);
+
+die();
 require_once('_language.php');
 require_once('_pdf copy.php');
 $pdf = new RECORDTCPDF(
